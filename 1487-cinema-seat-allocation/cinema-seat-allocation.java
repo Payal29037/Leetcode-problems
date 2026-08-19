05a388695f939512c9213012eb269946702f4e0e
Payal Kushwaha <payalkushwaha03@gmail.com>
2026-08-19 23:58:21 +0530

    Time: 16 ms (96.58%) | Memory: 53.9 MB (26.05%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.java
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.java
@@ -1,50 +1,20 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        Map<Integer, Integer>rows = new HashMap<>();
-
-        for (int[] r : reservedSeats){
-            int row = r[0];
-            int seat = r[1];
-
-            if(seat == 1 || seat == 10){
-                continue;
-            }
-
-            int mask = rows.getOrDefault(row,0);
-            mask |= 1 << seat;
-            rows.put(row, mask);
-
+        Map<Integer, Integer> d = new HashMap<>();
+        for (var e : reservedSeats) {
+            int i = e[0], j = e[1];
+            d.merge(i, 1 << (10 - j), (x, y) -> x | y);
         }
-        int total = (n - rows.size()) * 2;
-
-        int leftMask = 0;
-        int middleMask = 0;
-        int rightMask = 0;
-
-        for(int seat =2; seat <=5; seat++){
-            leftMask |= 1<<seat;
-        }
-
-        for(int seat =4; seat <= 7; seat++){
-            middleMask |= 1<< seat;
-        }
-
-        for(int seat =6; seat <= 9; seat++){
-            rightMask |=1 << seat;
-        }
-
-        for (int mask : rows.values()) {
-            boolean leftFree = (mask & leftMask) == 0;
-            boolean middleFree = (mask & middleMask) == 0;
-            boolean rightFree = (mask & rightMask) == 0;
-
-            if (leftFree && rightFree) {
-                total += 2;
-            } else if (leftFree || middleFree || rightFree) {
-                total += 1;
+        int[] masks = {0b0111100000, 0b0000011110, 0b0001111000};
+        int ans = (n - d.size()) * 2;
+        for (int x : d.values()) {
+            for (int mask : masks) {
+                if ((x & mask) == 0) {
+                    x |= mask;
+                    ++ans;
+                }
             }
         }
-        return total;
-
+        return ans;
     }
 }
